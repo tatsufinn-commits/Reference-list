dfa7ebd660319413a96ff0b41a2210d75f40356f
tatsufinn-commits <tatsufinn@gmail.com>
2026-07-22 11:07:24 +0800

    Delete MAA-Task-Bar-Hero-main directory

--- MAA-Task-Bar-Hero-main/Start MAA Task Bar Hero.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-
-if exist "C:\ProgramData\anaconda3\python.exe" (
-    "C:\ProgramData\anaconda3\python.exe" route_planner_gui.py
-) else (
-    py -3 route_planner_gui.py
-)
